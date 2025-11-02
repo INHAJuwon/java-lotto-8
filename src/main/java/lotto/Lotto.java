6556e93fd5213af3c08826697b06a101e13fdf20
ChoiJuwon <aowlrdofhdn@naver.com>
2025-11-03 00:12:19 +0900

    feat(lotto): 로또 한 장을 의미하는 Lotto 구현
    - 로또 한 장(6개의 번호)에서 생길 수 있는 유효성을 검사한다.
    - 로또끼리 몇 개의 번호가 일치하는지 확인한다.

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,59 @@
+package lotto;
+
+import java.util.*;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        checkDuplication(numbers);
+        Collections.sort(numbers);
+        checkRange(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    // TODO: 추가 기능 구현
+    private void checkDuplication(List<Integer> numbers) {
+        Set<Integer> compare = new HashSet<>(numbers);
+        if (compare.size() != numbers.size())
+            throw new IllegalArgumentException("[ERROR] 당첨 번호에는 중복값이 있을 수 없습니다.");
+    }
+
+    private void checkRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 1~45 사이의 값이어야 합니다.");
+            }
+        }
+    }
+
+    public int match(Lotto winning) {
+        int count = 0;
+
+        for (int number : winning.numbers) {
+            count += matchWinning(number);
+        }
+        return count;
+    }
+
+    private int matchWinning(int number) {
+        if (numbers.contains(number))
+            return 1;
+        return 0;
+    }
+
+    public boolean containBonus(int bonus) {
+        return numbers.contains(bonus);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
