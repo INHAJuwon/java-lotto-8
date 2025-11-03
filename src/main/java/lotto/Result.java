0bba2ba21fa26b9254a4d33a7c57ffa43c93f317
ChoiJuwon <aowlrdofhdn@naver.com>
2025-11-03 16:04:34 +0900

    feat(lotto): 로또의 전체적인 흐름을 제어하는 Result 구현

---
+++ src/main/java/lotto/Result.java
@@ -0,0 +1,44 @@
+package lotto;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+public class Result {
+    private static final int MODULUS = 10;
+    private final Map<LottoEnum, Integer> result;
+
+    public Result() {
+        this.result = new EnumMap<>(LottoEnum.class);
+        initializeMap();
+    }
+
+    public void calculateWinning(List<Lotto> lottoList, Lotto winning, int bonus) {
+        for (Lotto lotto : lottoList) {
+            int matchCount = lotto.match(winning);
+            boolean checkBonus = lotto.containBonus(bonus);
+            LottoEnum correct = LottoEnum.calculate(matchCount, checkBonus);
+            int correctCount = result.get(correct);
+
+            result.replace(correct, correctCount, correctCount + 1);
+        }
+    }
+
+    public Map<LottoEnum, Integer> getResult() {
+        return result;
+    }
+
+    private void initializeMap() {
+        for (LottoEnum rank : LottoEnum.values()) {
+            result.put(rank, 0);
+        }
+    }
+
+    public double calculateRevenue(int purchaseAmount) {
+        long totalPrize = 0;
+        for (Map.Entry<LottoEnum, Integer> entry : result.entrySet()) {
+            totalPrize += entry.getKey().getPrizeMoney() * entry.getValue();
+        }
+        return (double) totalPrize / purchaseAmount / MODULUS;
+    }
+}
